74416fc5671e571b424f2d5c094ddc48d1c7918a
berkayzaimdev <berkayzaimdev@gmail.com>
2024-08-12 15:02:13 +0300

    Test

---
+++ src/Ders_02/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        // Thread thread1 = new(ThreadMethod);
+        Thread thread2 = new((o) => 
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"Worker Thread -> {i}");
+            }
+        });
+
+        thread2.Start(); // thread koşmaya başlar
+
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine($"Main Thread -> {i}");
+        }
+    }
+
+    static void ThreadMethod()
+    {
+        // ...
+    }
+}
