244ab15e9bd72d29f719750d4517e5949d5eb884
Berkay Zaim <berkayzaimdev@gmail.com>
2024-12-24 07:35:26 +0300

    added iasyncenumerable and iasyncenumerator

---
+++ src/C18_IAsyncEnumerable_IAsyncEnumerator/Program.cs
@@ -0,0 +1,39 @@
+﻿#region Senkron
+
+// IEnumerable<int> GetNumbers()
+// {
+//     for (int i = 0; i < 10; i++)
+//     {
+//         Thread.Sleep(1000);
+//         yield return i;
+//     }
+// }
+//
+// foreach (var i in GetNumbers())
+// {
+//     Console.WriteLine(i);
+// }
+
+#endregion
+
+#region Asenkron
+
+// Büyük veri parçalarını işleme,
+// Büyük veri okuma-yazma süreçleri,
+// Web API ve uzak sunucu gibi kaynaklardan veri tüketme gibi durumlarda elverişli interface'lerdir.
+
+async IAsyncEnumerable<int> GetNumbersAsync()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        await Task.Delay(1000);
+        yield return i;
+    }
+}
+
+await foreach (var i in GetNumbersAsync())
+{
+    Console.WriteLine(i);
+}
+
+#endregion
